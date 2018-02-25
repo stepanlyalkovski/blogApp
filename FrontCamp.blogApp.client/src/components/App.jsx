1fd2b3c1e971af920b61440410013503ce973c61
stepanlyalkovski <stepanlyalkovski@gmail.com>
2018-02-25 18:13:26 +0300

    add app.jsx

--- FrontCamp.blogApp.client/src/components/App.jsx
+++ FrontCamp.blogApp.client/src/components/App.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default class App extends React.Component {
   render() {
 
+    const testTitle = () => 'FrontCamp initial project';
     return (
       <div style={{textAlign: 'center'}}>
+        <h1>{testTitle()}</h1>
       </div>);
   }
 }
